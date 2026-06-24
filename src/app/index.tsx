bf458c0b02705fa7c21f6fd34fc611384d66462a
Lahmar22 <lahmarzakariae22@gmail.com>
2026-06-24 12:33:39 +0100

    feat : create the project calculator

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,98 +1,234 @@
-import * as Device from 'expo-device';
-import { Platform, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-
-import { AnimatedIcon } from '@/components/animated-icon';
-import { HintRow } from '@/components/hint-row';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { WebBadge } from '@/components/web-badge';
-import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
-
-function getDevMenuHint() {
-  if (Platform.OS === 'web') {
-    return <ThemedText type="small">use browser devtools</ThemedText>;
-  }
-  if (Device.isDevice) {
-    return (
-      <ThemedText type="small">
-        shake device or press <ThemedText type="code">m</ThemedText> in terminal
-      </ThemedText>
-    );
-  }
-  const shortcut = Platform.OS === 'android' ? 'cmd+m (or ctrl+m)' : 'cmd+d';
-  return (
-    <ThemedText type="small">
-      press <ThemedText type="code">{shortcut}</ThemedText>
-    </ThemedText>
-  );
-}
+import { useState } from "react";
+import { View, Text, StyleSheet, TextInput, Button, TouchableOpacity } from "react-native";
 
 export default function HomeScreen() {
+
+  const [display, setDisplay] = useState("");
+
+  const addNumber = (value: string) => {
+    setDisplay(prev => prev + value);
+  };
+
+  const calculate = () => {
+    try {
+      const parts = display.split(/[+\-*/]/);
+      const operator = display.match(/[+\-*/]/)?.[0];
+
+      const a = Number(parts[0]);
+      const b = Number(parts[1]);
+
+      let result = 0;
+
+      switch (operator) {
+        case "+":
+          result = a + b;
+          break;
+        case "-":
+          result = a - b;
+          break;
+        case "/":
+          if (b === 0) {
+            throw new Error("Impossible de diviser par zero");
+          }
+
+          result = a / b;
+          break;
+        case "*":
+          result = a * b;
+          break;
+        default:
+          return;
+
+      }
+      setDisplay(String(result));
+
+    } catch(error) {
+      setDisplay(String(error));
+    }
+  };
+
+  const clear = () => {
+    setDisplay("");
+  };
+
   return (
-    <ThemedView style={styles.container}>
-      <SafeAreaView style={styles.safeArea}>
-        <ThemedView style={styles.heroSection}>
-          <AnimatedIcon />
-          <ThemedText type="title" style={styles.title}>
-            Welcome to&nbsp;Expo
-          </ThemedText>
-        </ThemedView>
+    <View style={styles.container}>
 
-        <ThemedText type="code" style={styles.code}>
-          get started
-        </ThemedText>
+      <TextInput
+        style={styles.input}
+        placeholder="0"
+        keyboardType="numeric"
+        editable={false}
+        value={display || "0"}
+      />
 
-        <ThemedView type="backgroundElement" style={styles.stepContainer}>
-          <HintRow
-            title="Try editing"
-            hint={<ThemedText type="code">src/app/index.tsx</ThemedText>}
-          />
-          <HintRow title="Dev tools" hint={getDevMenuHint()} />
-          <HintRow
-            title="Fresh start"
-            hint={<ThemedText type="code">npm run reset-project</ThemedText>}
-          />
-        </ThemedView>
+      <View style={styles.keyboard}>
 
-        {Platform.OS === 'web' && <WebBadge />}
-      </SafeAreaView>
-    </ThemedView>
+        <View style={styles.btnC}>
+          <TouchableOpacity style={styles.key} onPress={() => addNumber("1")}><Text>1</Text></TouchableOpacity>
+          <TouchableOpacity style={styles.key} onPress={() => addNumber("2")}><Text>2</Text></TouchableOpacity>
+          <TouchableOpacity style={styles.key} onPress={() => addNumber("3")}><Text>3</Text></TouchableOpacity>
+          <TouchableOpacity style={styles.key} onPress={() => addNumber("4")}><Text>4</Text></TouchableOpacity>
+        </View>
+
+        <View style={styles.btnC}>
+          <TouchableOpacity style={styles.key} onPress={() => addNumber("5")}><Text>5</Text></TouchableOpacity>
+          <TouchableOpacity style={styles.key} onPress={() => addNumber("6")}><Text>6</Text></TouchableOpacity>
+          <TouchableOpacity style={styles.key} onPress={() => addNumber("7")}><Text>7</Text></TouchableOpacity>
+          <TouchableOpacity style={styles.key} onPress={() => addNumber("8")}><Text>8</Text></TouchableOpacity>
+        </View>
+
+        <View style={styles.btnC}>
+          <TouchableOpacity style={styles.key} onPress={() => addNumber("9")}><Text>9</Text></TouchableOpacity>
+          <TouchableOpacity style={styles.key} onPress={() => addNumber("0")}><Text>0</Text></TouchableOpacity>
+          <TouchableOpacity style={styles.operatorKey} onPress={() => addNumber("+")}>
+            <Text style={styles.operatorText}>+</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.operatorKey} onPress={() => addNumber("-")}>
+            <Text style={styles.operatorText}>-</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.btnC}>
+          <TouchableOpacity style={styles.operatorKey} onPress={() => addNumber("/")}>
+            <Text style={styles.operatorText}>÷</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.operatorKey} onPress={() => addNumber("*")}>
+            <Text style={styles.operatorText}>×</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.operatorKey} onPress={() => addNumber(".")}>
+            <Text style={styles.operatorText}>.</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.operatorKeyC}
+            onPress={calculate}
+          >
+            <Text style={styles.btnText}>
+              =
+            </Text>
+          </TouchableOpacity>
+
+        </View>
+
+
+        <View style={styles.actions}>
+          <TouchableOpacity
+            style={styles.clearBtn}
+            onPress={clear}
+          >
+            <Text style={styles.btnText}>
+              C
+            </Text>
+          </TouchableOpacity>
+
+        </View>
+
+      </View>
+
+
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    flexDirection: 'row',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#ffffff",
   },
-  safeArea: {
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    alignItems: 'center',
-    gap: Spacing.three,
-    paddingBottom: BottomTabInset + Spacing.three,
-    maxWidth: MaxContentWidth,
-  },
-  heroSection: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    gap: Spacing.four,
-  },
+
   title: {
-    textAlign: 'center',
+    fontSize: 28,
+    fontWeight: "bold",
+    color: "blue",
+    marginBottom: 20,
   },
-  code: {
-    textTransform: 'uppercase',
+
+  input: {
+    height: 100,
+    fontWeight: "bold",
+    fontSize: 20,
+
   },
-  stepContainer: {
-    gap: Spacing.three,
-    alignSelf: 'stretch',
-    paddingHorizontal: Spacing.three,
-    paddingVertical: Spacing.four,
-    borderRadius: Spacing.four,
+
+  result: {
+    marginTop: 20,
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "green",
   },
+  keyboard: {
+    width: "90%",
+    alignItems: "center",
+    marginTop: 20,
+  },
+
+  btnC: {
+    flexDirection: "row",
+    gap: 15,
+    marginBottom: 15,
+  },
+
+
+  key: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    backgroundColor: "#f1f1f1",
+    justifyContent: "center",
+    alignItems: "center",
+    elevation: 4,
+  },
+
+
+  actions: {
+    flexDirection: "row",
+    gap: 20,
+    marginTop: 20,
+  },
+
+
+  clearBtn: {
+    backgroundColor: "red",
+    padding: 15,
+    borderRadius: 15,
+    width: 120,
+    alignItems: "center",
+  },
+
+
+  btnText: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+
+  operatorKey: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    backgroundColor: "#222",
+    justifyContent: "center",
+    alignItems: "center",
+    elevation: 5,
+  },
+  operatorKeyC: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    backgroundColor: "green",
+    justifyContent: "center",
+    alignItems: "center",
+    elevation: 5,
+  },
+
+  operatorText: {
+    color: "white",
+    fontSize: 28,
+    fontWeight: "bold",
+  },
+
 });
